d163405ec663e73a396da249833bb17a9f303cc6
Hana <2575735053@qq.com>
2023-05-31 15:03:50 +0800

    first commit

---
+++ vue-big-screen-plugin-master/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="app">
+    <router-view />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.app {
+  width: 100vw;
+  height: 100vh;
+  background-color: #020308;
+  background-image: url("./assets/背景.png");
+  background-size: cover;
+  background-position: center center;
+  overflow: hidden !important;
+}
+</style>
